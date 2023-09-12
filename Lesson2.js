176e1a7dc82bf07eb998cd2ba454010062ba7c50
savamix2013 <144225141+savamix2013@users.noreply.github.com>
2023-09-12 08:32:45 +0300

    Create Lesson2.js

---
+++ Lesson2.js
@@ -0,0 +1,17 @@
+let arr_syb = []
+
+arr_syb[0] = parseInt('1997', 10)
+arr_syb[1] = 27.97
+arr_syb[2] = "Всеволод Паращин"
+arr_syb[3] = "M"
+arr_syb[4] = 6 / 0
+
+let User_1 =
+{
+    name_u : arr_syb[2],
+    age : arr_syb[0],
+    Sex : arr_syb[3]
+}
+
+console.table({arr_syb})
+console.table({User_ME})
